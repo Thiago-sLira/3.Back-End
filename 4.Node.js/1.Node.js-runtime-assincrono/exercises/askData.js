9db8c66faff57d38baec897dbfbc7c5ddec531ae
Thiago Lira <thiago.slira@outlook.com>
2023-03-08 14:16:26 -0300

    atualizando branch principal

--- 4.Node.js/1.Node.js-runtime-assincrono/exercises/askData.js
+++
@@ -1,9 +0,0 @@
-const readLine = require('readline-sync');
-
-const askHeight = () => readLine.question('Qual sua altura em cm? \n');
-const askWeight = () => readLine.questionFloat('Qual seu peso em kg? \n');
-
-module.exports = {
-    askHeight,
-    askWeight,
-};
